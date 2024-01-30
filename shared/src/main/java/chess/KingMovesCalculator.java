da6b4f23bbcff03a84c1790ac16cbf592be8a1f2
Tristan Timothy <tristantimothy084@gmail.com>
2024-01-29 22:33:11 -0700

    Phase 1 start

---
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class KingMovesCalculator implements PieceMovesCalculator{
+    public Collection<ChessMove> moves(){
+        Collection<ChessMove> validMoves = new HashSet<>();
+        return validMoves;
+    }
+}
