6beb4c9a7d8858dcec806c6cf416c81b691ec93e
Tristan Timothy <tristantimothy084@gmail.com>
2024-04-03 21:13:43 -0600

    send message

--- server/src/main/java/server/WebSocketServer.java
+++ server/src/main/java/server/WebSocketServer.java
@@ -1,22 +1,28 @@
 package server;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import dataAccess.AuthDAO;
 import dataAccess.GameDAO;
 import dataAccess.SQLAuthDAO;
 import dataAccess.SQLGameDAO;
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+
 import record.AuthData;
 import spark.Spark;
+import webSocketMessages.serverMessages.Notification;
 import webSocketMessages.userCommands.JoinPlayer;
 import webSocketMessages.userCommands.UserGameCommand;
 
-import javax.websocket.Session;
+import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.annotations.*;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
+@WebSocket
 public class WebSocketServer {
     //gameID, authtokens of users in game
     Map<String, HashSet<String>> gameMap = new HashMap<>();
@@ -37,7 +43,7 @@ public class WebSocketServer {
         //Connection conn = null;
         if (conn != null) {
             switch (command.getCommandType()) {
-                case JOIN_PLAYER -> join(conn, msg);
+                //case JOIN_PLAYER -> join(conn, msg);
                 //case JOIN_OBSERVER -> observe(conn, msg);
                 //case MAKE_MOVE -> move(conn, msg));
                 //case LEAVE -> leave(conn, msg);
@@ -48,15 +54,31 @@ public class WebSocketServer {
         }
     }
     private String getConnection(String auth, Session session){
-
+        return null;
     }
-    private void join(Connection conn, String msg, Session session){
+    private void join(Connection conn, String msg, Session session) throws IOException {
         JoinPlayer command = new Gson().fromJson(msg, JoinPlayer.class);
         sessionMap.put(command.getAuthString(),session);
+        AuthDAO authy = new SQLAuthDAO();
+        AuthData data = authy.getAuth(command.getAuthString());
+        String username = data.getUsername();
+        String color;
+        if(command.playerColor== ChessGame.TeamColor.WHITE){
+            color = "White";
+        }
+        else{
+            color = "Black";
+        }
+        String output = username + " has joined as the " + color + " player";
         if(gameMap.containsKey(command.gameId)){
             HashSet<String> players = gameMap.get(command.gameId);
-            for(String user:players){
-
+            command.getAuthString();
+            if(!players.isEmpty()) {
+                for (String user : players) {
+                    Session userSession = sessionMap.get(user);
+                    //Notification notif = new Notification();
+                    userSession.getRemote().sendString(" ");
+                }
             }
             players.add(command.getAuthString());
         }
